d4f0747c0d79f6fb55088553332c7ae239c316bc
borbely-dominik-peter <98750586+borbely-dominik-peter@users.noreply.github.com>
2022-03-26 04:57:01 -0700

    Update urlap.js

--- urlap/urlap.js
+++ urlap/urlap.js
@@ -1,6 +1,48 @@
-      function valueadder(){
-       var test = document.getElementById('o11').innerHTML = "(3)";
-   }
-   function replacer() {
+   
+function replacer() {
     location.replace("urlap2.html");
-   }
+}
+function counter() {
+    document.getElementById('o11') = a
+    var a = 1
+    document.getElementById('o12') = b
+    var b = 0
+    document.getElementById('o13') = c
+    var c = 0
+    document.getElementById('o14') = d
+    var d = 0
+    document.getElementById('o21') = e
+    var e = 1
+    document.getElementById('o22') = f
+    var f = 0
+    document.getElementById('o23') = g
+    var g = 0
+    document.getElementById('o24') = h
+    var h = 0
+    document.getElementById('o31') = i
+    var i = 1
+    document.getElementById('o32') = j
+    var j = 0
+    document.getElementById('o33') = k
+    var k = 0
+    document.getElementById('o34') = l
+    var l = 0
+    document.getElementById('o41') = y
+    var y = 0
+    document.getElementById('o42') = x
+    var x = 0
+    document.getElementById('o43') = v
+    var v = 1
+    document.getElementById('o44') = n
+    var n = 0
+    document.getElementById('o51') = q
+    var q = 1
+    document.getElementById('o52') = w
+    var w = 0
+    document.getElementById('o53') = p
+    var p = 0
+    document.getElementById('o54') = m
+    var m = 0
+    var calc = 55
+    document.getElementById('result').innerHTML = calc;
+}
